53d9ed2d9a7fc80db8b151095772314e17c544f5
bleemesser <54053294+bleemesser@users.noreply.github.com>
2023-04-24 11:02:28 -0700

    pagerank

--- main.go
+++ main.go
@@ -1,152 +1,27 @@
 package main
 
 import (
-	// alg "algorithms_class/algorithms"
-	ds "algorithms_class/datastructures"
-	"sync"
-
-	"fmt"
-	"math/rand"
-
-	"github.com/schollz/progressbar"
-
-	// "strconv"
-
-	// "strings"
-	"time"
-
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/plotutil"
-	"gonum.org/v1/plot/vg"
+	pg "algorithms_class/pagerank"
 )
 
-func runTest(iters int, findMinIters int, insertionSizes []int, findMinSizes []int) (map[int]time.Duration, map[int]time.Duration) {
-	var constructionLock sync.Mutex
-	insertionTimes := make(map[int]time.Duration)
-	findMinTimes := make(map[int]time.Duration)
-
-	// create a new progress bar for the trial loop
-	bar := progressbar.New(iters * len(insertionSizes))
-
-	// create a progress bar for constructing the trees
-	bar2 := progressbar.New(len(insertionSizes))
-
-	// create a progress bar for finding the min
-	bar3 := progressbar.New(len(findMinSizes) * findMinIters)
-	// create random trees for each size, use multiple threads
-	trees := make(map[int]*ds.Node)
-	var cg sync.WaitGroup
-	for _, v := range insertionSizes {
-		cg.Add(1)
-		go func(v int) {
-			defer cg.Done()
-			defer bar2.Add(1)
-			tree := ds.AvlTree(rand.Perm(v))
-			constructionLock.Lock()
-			trees[v] = tree
-			constructionLock.Unlock()
-		}(v)
-
-	}
-	cg.Wait()
-
-	bar.Reset()
-	fmt.Println(trees)
-	for i := 0; i < iters; i++ {
-		for size, v := range trees {
-			// insert
-			toInsert := rand.Intn(size)
-			start := time.Now()
-			v.InsertNode(toInsert)
-			elapsed := time.Since(start)
-			insertionTimes[size] += elapsed
-			v.DeleteNode(toInsert)
-			bar.Add(1)
-		}
-	}
-
-	// find min
-	for i := 0; i < findMinIters; i++ {
-		for _, size := range findMinSizes {
-			tree := ds.AvlTree(rand.Perm(size))
-			start := time.Now()
-			ds.FindAVLMin(tree)
-			elapsed := time.Since(start)
-			findMinTimes[size] += elapsed
-			bar3.Add(1)
-		}
-	}
-
-	for k, v := range insertionTimes {
-		insertionTimes[k] = v / time.Duration(iters)
-	}
-
-	for k, v := range findMinTimes {
-		findMinTimes[k] = v / time.Duration(findMinIters)
-	}
-
-	return insertionTimes, findMinTimes
-}
-
 func main() {
-	// array from 1000 to 40 million in doubling incrememts 1000, 2000, 4000, etc
-	insertionSizes := make([]int, 0)
-	for i := 1000; i <= 40000000; i *= 2 {
-		insertionSizes = append(insertionSizes, i)
-	}
-	// array from 1000 to 100,000 in increments of 1000
-	findMinSizes := make([]int, 0)
-	for i := 100000; i <= 1000000; i += 100000 {
-		findMinSizes = append(findMinSizes, i)
-	}
-	fmt.Println(insertionSizes)
-	fmt.Println(findMinSizes)
-	iters := 2000000
-	findMinIters := 100
-	insertionAvg, findminAvg := runTest(iters, findMinIters, insertionSizes, findMinSizes)
+	pr := pg.MakePageRank(0.85, 35, 0.25, false)	
+	pageA := pr.AddPage("A")
+	pageB := pr.AddPage("B")
+	pageC := pr.AddPage("C")
+	pageD := pr.AddPage("D")
 
-	// plot
-	p := plot.New()
-	p.Title.Text = "AVL Tree Insertion time vs tree size"
-	p.X.Label.Text = "Tree Size"
-	p.Y.Label.Text = "Time (ns)"
+	pr.AddLink(pageA, pageB)
+	pr.AddLink(pageA, pageC)
+	pr.AddLink(pageB, pageC)
+	pr.AddLink(pageC, pageA)
+	pr.AddLink(pageD, pageC)
 
-	pts := make(plotter.XYs, len(insertionSizes))
+	pr.Run()
 
-	for i, v := range insertionSizes {
-		pts[i].X = float64(v)
-		pts[i].Y = float64(insertionAvg[v].Nanoseconds())
-	}
+	pr.PrintToFile("pagerank.txt")
 
-	err := plotutil.AddLinePoints(p, "Insertion", pts)
-	if err != nil {
-		panic(err)
-	}
-	// high res plot
-	if err := p.Save(16*vg.Inch, 16*vg.Inch, "insertion.png"); err != nil {
-		panic(err)
-	}
 
-	p2 := plot.New()
-	p2.Title.Text = "AVL Tree FindMin vs tree size"
-	p2.X.Label.Text = "Tree Size"
-	p2.Y.Label.Text = "Time (ns)"
-
-	pts2 := make(plotter.XYs, len(findMinSizes))
-
-	for i, v := range findMinSizes {
-		pts2[i].X = float64(v)
-		pts2[i].Y = float64(findminAvg[v].Nanoseconds())
-	}
-
-	err = plotutil.AddLinePoints(p2, "FindMin", pts2)
-	if err != nil {
-		panic(err)
-	}
-
-	if err := p2.Save(16*vg.Inch, 16*vg.Inch, "findMin.png"); err != nil {
-		panic(err)
-	}
+	// see output file, results are correct to the example.
 
 }
--- pagerank/pagerank.go
+++ pagerank/pagerank.go
@@ -1,5 +1,10 @@
 package pagerank
 
+import (
+	"fmt"
+	"os"
+)
+
 // PR(A) = (1-d) + d(PR(T1)/C(T1) + ... + PR(Tn)/C(Tn))
 // PR() is the PageRank
 // A is the page
@@ -14,58 +19,117 @@ type Page struct {
 	Rank      float64
 }
 
-func (p *Page) AddLink(to int) {
-	p.Links = append(p.Links, to)
-}
-
-func (p *Page) AddBackLink(from int) {
-	p.BackLinks = append(p.BackLinks, from)
+func NewPage(name string) Page {
+	return Page{Name: name, Links: []int{}, BackLinks: []int{}, Rank: 0.0}
 }
 
 func (p *Page) CalculateRank(pages []Page, dampingFactor float64) float64 {
-	rank := (1.0 - dampingFactor)
-	for _, i := range p.BackLinks {
-		rank += dampingFactor * (pages[i].Rank / float64(len(pages[i].Links)))
+	fmt.Println("Page ", p.Name, " has rank ", p.Rank, " and ", len(p.BackLinks), " backlinks")
+	sum := 0.0
+	for bl := range p.BackLinks {
+		fmt.Println("Calculating rank using backlink ", pages[p.BackLinks[bl]].Name, " with rank ", pages[p.BackLinks[bl]].Rank, " and ", len(pages[p.BackLinks[bl]].Links), " links")
+		sum += pages[p.BackLinks[bl]].Rank / float64(len(pages[p.BackLinks[bl]].Links))
 	}
-	return rank
+	out := (1 - dampingFactor) + dampingFactor*sum
+	fmt.Println("Page ", p.Name, " has new rank ", out)
+	return out
 }
 
-type PageRank struct {
+type PageRank struct { // this is organized so only the pagerank struct can modify its data
 	DF         float64
 	Pages      []Page
 	Iterations int
+	InitRank   float64
+	HasSuper   bool
 }
 
-func (pr *PageRank) AddPage(name string) int {
-	p := Page{Name: name, Index: len(pr.Pages), Rank: 1.0}
-	pr.Pages = append(pr.Pages, p)
-	return p.Index
+func (pr *PageRank) UpdatePage(index int, page Page) {
+	pr.Pages[index] = page
 }
 
-func (pr *PageRank) AddLink(from, to int) {
-	pr.Pages[from].AddLink(to)
-	pr.Pages[to].AddBackLink(from)
-}
-
-func MakePageRank(dampingFactor float64, iterations int, pages []Page, initialRank float64, addSuperNode bool) PageRank {
-	pr := PageRank{}
-	pr.DF = dampingFactor
-	pr.Iterations = iterations
-
-	pr.Pages = pages
-	
-	for i := range pr.Pages {
-		pr.Pages[i].Rank = initialRank
-	}
-
+func MakePageRank(dampingFactor float64, iterations int, initialRank float64, addSuperNode bool) PageRank {
+	pr := PageRank{DF: dampingFactor, Pages: []Page{}, Iterations: iterations, InitRank: initialRank, HasSuper: addSuperNode}
 	if addSuperNode {
-		superNode := pr.AddPage("SuperNode")
-		for i := range pr.Pages {
-			pr.AddLink(superNode, i)
-			pr.AddLink(i, superNode)
-		}
+		pr.AddSuperNode()
 	}
-
 	return pr
 }
 
+func (pr *PageRank) AddSuperNode() {
+	page := NewPage("SuperNode")
+	page.Index = 0
+	page.Rank = 1.0
+	page.Links = []int{}
+	page.BackLinks = []int{}
+	pr.Pages = append(pr.Pages, page)
+}
+
+func (pr *PageRank) AddPage(name string) Page {
+	page := NewPage(name)
+	page.Index = len(pr.Pages)
+	page.Rank = pr.InitRank
+	page.Links = []int{}
+	page.BackLinks = []int{}
+	pr.Pages = append(pr.Pages, page)
+
+	// if we have a super node, add a link from it to this page
+
+	if pr.HasSuper {
+		superNode := pr.Pages[0] // super node is always at index 0
+		superNode.Links = append(superNode.Links, page.Index)
+		page.BackLinks = append(page.BackLinks, superNode.Index)
+		pr.UpdatePage(0, superNode)
+		pr.UpdatePage(page.Index, page)
+	}
+
+	return page
+}
+
+func (pr *PageRank) AddLink(from, to Page) {
+	// fmt.Println("Adding link from ", from.Name, " to ", to.Name)
+	// fmt.Println("from index: ", from.Index, " to index: ", to.Index)
+
+	rfrom := pr.Pages[from.Index]
+	rto := pr.Pages[to.Index]
+
+	rfrom.Links = append(rfrom.Links, to.Index)
+	rto.BackLinks = append(rto.BackLinks, from.Index)
+	
+	pr.UpdatePage(from.Index, rfrom)
+	pr.UpdatePage(to.Index, rto)
+
+	// fmt.Println("Finished adding link from ", from.Name, " to ", to.Name)
+	// fmt.Println("FROM LINKS: ", pr.Pages[from.Index].Links)
+	// fmt.Println("TO BACKLINKS: ", pr.Pages[to.Index].BackLinks)
+}
+
+func (pr *PageRank) Run() {
+	f, err := os.Create("pagerank.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	for i := 0; i < pr.Iterations; i++ {
+		fmt.Fprintf(f, fmt.Sprintf("Iteration: %d\n", i))
+		for i := range pr.Pages {
+			pr.Pages[i].Rank = pr.Pages[i].CalculateRank(pr.Pages, pr.DF)
+			fmt.Fprintf(f, fmt.Sprintf("%s: Rank: %f, Index: %v, Links: %v, Backlinks: %v\n", pr.Pages[i].Name, pr.Pages[i].Rank, pr.Pages[i].Index, pr.Pages[i].Links, pr.Pages[i].BackLinks))
+		}
+		fmt.Println("Iteration: ", i)
+	}
+}
+
+// print to file
+func (pr *PageRank) PrintToFile(filename string) {
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	for i := range pr.Pages {
+		f.WriteString(fmt.Sprintf("After %v iterations:\n%s: Rank: %f, Index: %v, Links: %v, Backlinks: %v\n", pr.Iterations, pr.Pages[i].Name, pr.Pages[i].Rank, pr.Pages[i].Index, pr.Pages[i].Links, pr.Pages[i].BackLinks))
+	}
+}
